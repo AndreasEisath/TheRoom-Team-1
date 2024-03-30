d81b0845749d317df6177802f2cbe70dd1be0142
Tobi-AA <aabdu245@uottawa.ca>
2024-03-30 13:47:50 -0400

    Make subtitles toggle-able, stop when TV is turned off

--- Assets/SubtitleData/SubtitleData.cs
+++ Assets/SubtitleData/SubtitleData.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class SubtitleData : MonoBehaviour
 {
     public SubtitleManager subtitleManager;
     [TextArea]
     public List<string> subtitleText = new(1);
+    public PlayableDirector director;
     private int index = -1;
     // Start is called before the first frame update
     void Start()
@@ -15,17 +17,32 @@ public class SubtitleData : MonoBehaviour
         {
             subtitleManager = Object.FindObjectOfType<SubtitleManager>();
         }
+        if(director == null)
+        {
+            director = GetComponent<PlayableDirector>();
+        }
     }
 
+    public void ToggleSubtitles()
+    {
+        if(director.state == PlayState.Playing)
+        {
+            director.Stop();
+            ResetSubtitle(true);
+            return;
+        }
+        director.Play();
+    }
     public void ResetSubtitle(bool reset)
     {
         if (reset) index = -1;
         subtitleManager.HideSubtitles();
+        GetComponent<PlayableDirector>().Stop();
     }
     public void TickSubtitle()
     {
         index += 1;
-        if (index > subtitleText.Count)
+        if (index >= subtitleText.Count)
         {
             // No more subtitles, disable and stop
             ResetSubtitle(true);
